8d4edd347f749d79b0243621dc1af316c9223586
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-09 12:27:06 +0300

    Добавьте файлы проекта.

---
+++ Dokyment.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Document {
+public:
+    virtual void create() = 0;
+    virtual void open(const string& filename) = 0;
+    virtual void save() = 0;
+    virtual void saveAs(const string& filename) = 0;
+    virtual void print() = 0;
+    virtual void close() = 0;
+};
+
+
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+
+
+    return 0;
+}
